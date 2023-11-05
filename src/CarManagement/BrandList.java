96d1ab4e595545031506995dfd40e7269238b216
Phien47 <146061265+Phien47@users.noreply.github.com>
2023-11-05 22:40:57 +0700

    BrandList add function

--- src/CarManagement/BrandList.java
+++ src/CarManagement/BrandList.java
@@ -51,6 +51,115 @@ public class BrandList extends ArrayList<Brand> {
         }
         return false;
     }
-    
+    public int searchID(String bID) {
+        if (this.isEmpty()) {
+            return -1;
+        }
+        for (int i = 0; i < this.size(); i++) {
+            if (bID.equals(this.get(i).getBrandID())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //chuyển list thành 1 menu cho người dùng chọn 1 brand từ đây
+    public Brand getUserChoice() {
+        Menu menu = new Menu();
+        return (Brand) menu.ref_getChoice(this);
+    }
+
+    public void addBrand() {
+        int pos;
+        do {
+            brandID = Functions.getID("Input brand ID: ", "The brandID must not be null. Try again !");
+            pos = searchID(brandID);
+            if (pos >= 0) {
+                System.out.println("This brand ID is existed. Try another one!");
+            }
+        } while (pos != -1);
+
+        brandName = Functions.getString("Input brand name: ", "The brand name must not be null. Try again !");
+        soundBrand = Functions.getString("Input sound brand: ", "The sound brand must not be null. Try again !");
+        price = Functions.getADouble("Input price: ", "The price must be a number and greater than 0. Try again !", 0);
+        this.add(new Brand(brandID, brandName, soundBrand, price));
+        brandIDList.add(brandID);
+        System.out.println("Brand has been added successfully");
+    }
+
+    public void updateBrand() {
+        Menu menu = new Menu("Please choose the characteristics of brand that you want to update");
+        menu.addNewOption("1. Update brand name");
+        menu.addNewOption("2. Update sound brand");
+        menu.addNewOption("3. Update price");
+        menu.addNewOption("4. Update all");
+        menu.addNewOption("5. Exit");
+        int choice;
+        String exitPoint = "Y";
+        brandID = Functions.getID("Input brand ID: ", "The brandID must not be null. Try again !");
+        int pos = searchID(brandID);
+        if (pos == -1) {
+            System.out.println("Not found !");
+        } else {
+            do {
+                Brand x = this.get(pos);
+                System.out.println("Here is the Brand before updating");
+                System.out.println(x);
+                menu.printMenu();
+                choice = menu.int_getChoice();
+
+                switch (choice) {
+                    case 1:
+                        brandName = Functions.getString("Input brand name: ", "The brand name must not be null. Try again !");
+                        x.setBrandName(brandName);
+                        System.out.println("Brand has been updated successfully !");
+                        System.out.println("Do you want to continue updating the brand with ID " + x.getBrandID());
+                        System.out.print("Choose(Y/N): ");
+                        exitPoint = sc.nextLine().toUpperCase();
+                        if (!exitPoint.equals("Y")) {
+                            exitPoint = "N";
+                        }
+                        break;
+                    case 2:
+                        soundBrand = Functions.getString("Input sound brand: ", "The sound brand must not be null. Try again !");
+                        x.setSoundBrand(soundBrand);
+                        System.out.println("Brand has been updated successfully !");
+                        System.out.println("Do you want to continue updating the brand with ID " + x.getBrandID());
+                        System.out.print("Choose(Y/N): ");
+                        exitPoint = sc.nextLine().toUpperCase();
+                        if (!exitPoint.equals("Y")) {
+                            exitPoint = "N";
+                        }
+                        break;
+                    case 3:
+                        price = Functions.getADouble("Input price: ", "The price must be a number and greater than 0. Try again !", 0);
+                        x.setPrice(price);
+                        System.out.println("Brand has been updated successfully !");
+                        System.out.println("Do you want to continue updating the brand with ID " + x.getBrandID());
+                        System.out.print("Choose(Y/N): ");
+                        exitPoint = sc.nextLine().toUpperCase();
+                        if (!exitPoint.equals("Y")) {
+                            exitPoint = "N";
+                        }
+                        break;
+                    case 4:
+                        brandName = Functions.getString("Input brand name: ", "The brand name must not be null. Try again !");
+                        soundBrand = Functions.getString("Input sound brand: ", "The sound brand must not be null. Try again !");
+                        price = Functions.getADouble("Input price: ", "The price must be a number and greater than 0. Try again !", 0);
+                        x.setBrandName(brandName);
+                        x.setSoundBrand(soundBrand);
+                        x.setPrice(price);
+                        System.out.println("Brand has been updated successfully !");
+                        System.out.println("Do you want to continue updating the brand with ID " + x.getBrandID());
+                        System.out.print("Choose(Y/N): ");
+                        exitPoint = sc.nextLine().toUpperCase();
+                        if (!exitPoint.equals("Y")) {
+                            exitPoint = "N";
+                        }
+                        break;
+                }
+            } while (choice >= 1 && choice <= 4 && exitPoint.equals("Y"));
+        }
+    }
     
 }
